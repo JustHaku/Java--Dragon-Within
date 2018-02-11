97f22a907b4f21dab8b530186048294145ccb212
valka <valka@10.32.174.233>
2018-02-11 21:50:43 +0000

    Modifying Basic skills and skills
    Basic skills can read from xml file.

--- src/Skills.java
+++ src/Skills.java
@@ -1,5 +1,8 @@
 
 import java.util.function.BiConsumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jdk.nashorn.internal.ir.CatchNode;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,60 +15,205 @@ import java.util.function.BiConsumer;
  */
 public class Skills {
 
+    public static int ALL = 0;
+    public static int FRIENDLY = 1;
+    public static int ENEMY = -1;
+
     private String name;
     private String description;
     private int cost;
     private int value;
 
-    private Character applier;
-    private Character applyee;
-    private BiConsumer<Character, Integer> consuming;
-    private BiConsumer<Character, Integer> applying;
-    public final boolean unary;
-    private boolean revertable;
+    Character applier;
+    private Character[] applyee;
+    private myBiConsumer<Character, Integer> consuming;
+    private myBiConsumer<Character, Integer> applying;
+    public final int affects;
 
-    public Skills(String name, int cost, int value, BiConsumer<Character, Integer> wayofpaying, 
-                  BiConsumer<Character, Integer> wayofapplying, boolean isUnary,boolean isRevertable) 
-    {
+    public final boolean unary;
+    public final boolean revertable;
+
+    /**
+     * Creates a new skill
+     *
+     * @param name of the skill
+     * @param cost cost for allying the skill
+     * @param value of the effect of the skill
+     * @param wayofpaying way of paying for the skill
+     * @param wayofapplying way of applying the skill
+     * @param affects the types of character it affects(use the static constant
+     * in this class to pass here)
+     * @param affected number of characters that this skill affects
+     * @param isUnary true if the skills is applied on the character casting it
+     * @param isRevertable if the skill can be reverted
+     */
+    public Skills(String name, int cost, int value, myBiConsumer<Character, Integer> wayofpaying,
+            myBiConsumer<Character, Integer> wayofapplying, int affects, int affected, boolean isUnary, boolean isRevertable) {
         this.name = name;
+        this.cost = cost;
+        this.value = value;
+
+        this.consuming = wayofpaying;
+        this.applying = wayofapplying;
+
         unary = isUnary;
         revertable = isRevertable;
-        
-        if(unary){
-            applyee = applier;
-        }
+
+        this.applyee = new Character[affected];
+        this.affects = affects;
     }
 
+    /**
+     * Return name of the skill
+     *
+     * @return name of the skill
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns description of the skill;
+     *
+     * @return description of the skill;
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Adds a character that this skill needs to be applied to
+     *
+     * @param applyee character that this skill needs to be applied to
+     */
     public void applyTo(Character applyee) {
-        this.applyee = applyee;
-    }
-    
-    public void teachTo(Character dude){
-        /*
-            applier = dude
-            dude.addSkill(this)\dude.learn(this)    
-        */
-    }
-    
-    public void revert() {
-        if (revertable){
-            applying.accept(applyee, -value);
-        }
-        if(!unary){
-            applyee = null;
+
+        for (int i = 0; i < this.applyee.length; i++) {
+            if (this.applyee[i] == null) {
+                this.applyee[i] = applyee;
+            }
         }
     }
 
-    public void executeSkill() {
-        consuming.accept(applier, cost);
-        applying.accept(applyee, value);
+    /**
+     * Removes all references to the characters that the skill would be casted
+     * on.
+     */
+    public void unBindAll() {
+        for (int i = 0; i < this.applyee.length; i++) {
+            this.applyee[i] = null;
+        }
+    }
+
+    /**
+     * sets a description for the skill
+     *
+     * @param desc
+     */
+    public void setDescription(String desc) {
+        this.description = desc;
+    }
+
+    /**
+     * use on of the static variables(ALL, FRIENLY, ENEMY) to determen on which
+     * characters can this be casted
+     *
+     * @param i one of [ALL, FRIENLY, ENEMY]
+     * @return true if can affect i
+     */
+    public boolean doesitAffect(int i) {
+        if (affects == ALL) {
+            return true;
+        } else {
+            return affects == i;
+        }
+    }
+
+    /**
+     * gets the reversed version of the skill
+     *
+     * @return
+     */
+    public Skills getReverted() {
+        if (revertable) {
+            this.value *= -1;
+            try {
+                Skills clone = (Skills) this.clone();
+                return clone;
+            } catch (CloneNotSupportedException e) {
+                return null;
+            } finally {
+                value *= -1;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * teaches a skill to a character
+     *
+     * @param dude
+     */
+    public void teachTo(Character dude) {
+        applier = dude;
+        dude.addSkill(this);
+    }
+
+    /**
+     * executes the skill on the characters that were added by the
+     * {@link Skills.applyTo}
+     *
+     * @throws Skills.NotEnoughSelectedException when there aren't enough
+     * characters selected to cast the skill on
+     * @throws Skills.NotEnoughResorcesToCastException when the caster has not
+     * got enough resources to cast the spell
+     */
+    public void executeSkill() throws NotEnoughSelectedException, NotEnoughResorcesToCastException {
+        try {
+            consuming.accept(applier, cost);
+        } catch (Exception ex) {
+            throw new NotEnoughResorcesToCastException();
+        }
+
+        try {
+            for (Character ap : applyee) {
+                applying.accept(ap, applier.attack + value);
+            }
+        } catch (Exception e) {
+            throw new NotEnoughSelectedException();
+
+        }
+    }
+
+    /**
+     *
+     * @param <T>
+     * @param <U>
+     */
+    @FunctionalInterface
+    public static interface myBiConsumer<T extends Character, U extends Integer> {
+
+        void accept(T t, U u) throws Exception;
+    }
+
+    /**
+     * Exception for when the the skill needs more characters to be selected
+     */
+    public static class NotEnoughSelectedException extends Exception {
+
+        public NotEnoughSelectedException() {
+            super("Not enough characters were selected");
+        }
+    }
+
+    /**
+     * Exception for when the character can't cast the spell because there isn't
+     * enough mana
+     */
+    public static class NotEnoughResorcesToCastException extends Exception {
+
+        public NotEnoughResorcesToCastException() {
+            super("not enough resorses to cast this skill");
+        }
     }
 }
