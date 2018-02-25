88a81bac0ea2f41b178f6849dc224a2e6389cd73
Georgi Valchanov <g.valchanov@lancaster.ac.uk>
2018-02-25 18:20:07 +0000

    Changed Skills, Basic Skills and skills.xml file.
    Skills now has two new constructers making the creation of a skill less verbose;
    Basic Skills now creates the skills differently depending on whether they are unary;
    New Skills added to the xml file;

--- src/Skills.java
+++ src/Skills.java
@@ -9,16 +9,16 @@ public class Skills {
     public static int FRIENDLY = 1;
     public static int ENEMY = -1;
 
-    private String name;
+    private final String name;
     private String description;
     private String posteffect;
     private int cost;
     private int value;
 
-    Character applier;
-    private Character[] targets;
-    private myBiConsumer<Character, Integer> consuming;
-    private myBiConsumer<Character, Integer> applying;
+    private Character applier;
+    private final Character[] targets;
+    private final myBiConsumer<Character, Integer> consuming;
+    private final myBiConsumer<Character, Integer> applying;
     public final int affects;
 
     public final boolean unary;
@@ -39,8 +39,9 @@ public class Skills {
      * @param isUnary true if the skills is applied on the character casting it
      * @param isRevertable if the skill can be reverted
      */
-    public Skills(String name, int cost, int value, myBiConsumer<Character, Integer> wayofpaying,
+    private Skills(String name, int cost, int value, myBiConsumer<Character, Integer> wayofpaying,
             myBiConsumer<Character, Integer> wayofapplying, int affects, int affected, boolean isUnary, boolean isRevertable) {
+
         this.name = name;
         this.cost = cost;
         this.value = value;
@@ -48,11 +49,47 @@ public class Skills {
         this.consuming = wayofpaying;
         this.applying = wayofapplying;
 
-        unary = isUnary;
         revertable = isRevertable;
 
-        this.targets = new Character[affected];
+        unary = isUnary;
+        if (unary) {
+            targets = new Character[1];
+        } else {
+            targets = new Character[affected];
+        }
+
         this.affects = affects;
+    }
+
+    /**
+     * Creates a new skill that is not unary
+     *
+     * @param name of the skill
+     * @param cost cost for allying the skill
+     * @param value of the effect of the skill
+     * @param wayofpaying way of paying for the skill
+     * @param wayofapplying way of applying the skill
+     * @param affects the types of character it affects(use the static constant
+     * in this class to pass here)
+     * @param affected number of characters that this skill affects
+     * @param isRevertable if the skill can be reverted
+     */
+    public Skills(String name, int cost, int value, myBiConsumer<Character, Integer> wayofpaying, myBiConsumer<Character, Integer> wayofapplying, int affects, int affected, boolean isRevertable) {
+        this(name, cost, value, wayofpaying, wayofapplying, affects, affected, false, isRevertable);
+    }
+
+    /**
+     * Creates a new skill that is unary
+     *
+     * @param name of the skill
+     * @param cost cost for allying the skill
+     * @param value of the effect of the skill
+     * @param wayofpaying way of paying for the skill
+     * @param wayofapplying way of applying the skill
+     * @param isRevertable if the skill can be reverted
+     */
+    public Skills(String name, int cost, int value, myBiConsumer<Character, Integer> wayofpaying, myBiConsumer<Character, Integer> wayofapplying, boolean isRevertable) {
+        this(name, cost, value, wayofpaying, wayofapplying, Skills.FRIENDLY, 1, true, isRevertable);
     }
 
     /**
@@ -107,8 +144,10 @@ public class Skills {
      * on.
      */
     public void unBindAll() {
-        for (int i = 0; i < this.targets.length; i++) {
-            this.targets[i] = null;
+        if (!unary) {
+            for (int i = 0; i < this.targets.length; i++) {
+                this.targets[i] = null;
+            }
         }
     }
 
@@ -131,8 +170,8 @@ public class Skills {
     }
 
     /**
-     * use on of the static variables(ALL, FRIENLY, ENEMY) to determine on which
-     * characters can this be casted
+     * use one of the static variables(ALL, FRIENLY, ENEMY) to determine on
+     * which characters can this be casted
      *
      * @param i one of [ALL, FRIENLY, ENEMY]
      * @return true if can affect i
@@ -151,7 +190,6 @@ public class Skills {
      * @return
      */
     public Runnable getReverted() {
-
         return () -> {
             try {
                 for (Character ap : targets) {
@@ -193,7 +231,7 @@ public class Skills {
             consuming.accept(applier, cost);
             consuming.accept(applier, -cost);
             return true;
-        } catch (Exception e) {
+        } catch (SkillExeption e) {
             return false;
         }
     }
@@ -202,66 +240,55 @@ public class Skills {
      * executes the skill on the characters that were added by the
      * {@link Skills.applyTo}
      *
-     * @throws Skills.NotEnoughSelectedException when there aren't enough
-     * characters selected to cast the skill on
-     * @throws Skills.NotEnoughResourcesToCastException when the caster has not
-     * got enough resources to cast the spell
+     * @throws Skills.SkillExeption when the skill cannot be casted
      */
-    public void executeSkill() throws NotEnoughSelectedException, NotEnoughResourcesToCastException {
-        try {
-            System.out.println(this.canItbeCast());
-            consuming.accept(applier, cost);
-        } catch (Exception ex) {
-            throw new NotEnoughResourcesToCastException();
-        }
+    public void executeSkill() throws SkillExeption {
+        consuming.accept(applier, cost);
 
+        //for damaging abilities the value retrieved from the xml file acts as a percentage
+        if (damaging) {
+            value = (applier.totalDmg() * value / 100) + applier.totalDmg();
+        }
         try {
-            //for damaging abilities the value retrieved from the xml file acts as a percentage
-            if (damaging) {
-                value = (applier.totalDmg() * value / 100) + applier.totalDmg();
-            }
+            // apply the effect on each target  
             for (Character ap : targets) {
                 applying.accept(ap, value);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new NotEnoughSelectedException();
+        } catch (NullPointerException e) {
+            throw new SkillExeption("Not enough targets");
         }
     }
 
+    /**
+     * Sets a flag that states that the skill would reduce health points
+     *
+     * @param d true if the skill should take hp
+     */
     public void setDamaging(boolean d) {
         this.damaging = d;
     }
 
     /**
+     * Used for describing the way of payment and away of applying of the skill
      *
-     * @param <T>
-     * @param <U>
+     * @param <T> A character that is going to pay for the skill or going to be
+     * a target of the skill;
+     * @param <U> Value of the skill
      */
     @FunctionalInterface
     public static interface myBiConsumer<T extends Character, U extends Integer> {
 
-        void accept(T t, U u) throws Exception;
+        void accept(T t, U u) throws SkillExeption;
     }
 
     /**
-     * Exception for when the the skill needs more characters to be selected
+     * Exception for when the caster can't cast the skill. Examples: when the
+     * caster doesn't have enough points to cast it; when there aren't enough
+     * targets for the skill to be cast
      */
-    public static class NotEnoughSelectedException extends Exception {
-
-        public NotEnoughSelectedException() {
-            super("Not enough characters were selected");
-        }
-    }
-
-    /**
-     * Exception for when the character can't cast the spell because there isn't
-     * enough mana
-     */
-    public static class NotEnoughResourcesToCastException extends Exception {
-
-        public NotEnoughResourcesToCastException() {
-            super("not enough resources to cast this skill");
+    public static class SkillExeption extends Exception {
+        public SkillExeption(String s) {
+            super(s);
         }
     }
 }
